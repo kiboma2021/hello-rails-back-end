233a4c06031a91f25d8907255d6d3ff85d4b62ed
kiboma <omayioben@gmail.com>
2024-02-15 08:37:45 +0300

    Add cors to open API for react

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://localhost:4000'
+      origins '*'
       resource '/random_greeting', headers: :any, methods: [:get]
     end
   end
