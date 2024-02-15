36f339a47f48999718c90892ba3f0a1b58d34c29
kiboma <omayioben@gmail.com>
2024-02-15 16:20:31 +0300

    Update the API to return one random message

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -3,8 +3,4 @@ class Api::MessagesController < ApplicationController
       render json: Message.order("RANDOM()").first
     end
 
-  # def fetch_all_messages
-  #   messages = Message.all
-  #   render json: messages
-  # end
 end
